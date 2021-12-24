94bb71aef8273ec8653b9cd953a0ee99f4f0c255
doctor-melamed <jakobmelamed@gmail.com>
2021-12-24 20:24:56 +0100

    Methoden zum Übersetzen

--- Alphabet.java
+++ Alphabet.java
@@ -1,16 +1,37 @@
 public class Alphabet {
 
-    public static translateToMorse(String word) {
+    public String translateToMorse(String word) {
+
+        String translationToMorse;
 
         for (int i = 0 ; i < word.length() ; i++) {
             for (int j = 0 ; j < 26 ; j++) {
-                
-                if(word[i].isEqualTo(letterList[j]))
-                {
-                    
+                //Überprüfung wann der Buchstabe der letterList entspricht.
+                if(word.equals(letterList[j])) {
+                    //Der entsprechende Morsecode des Buchstaben wird in den String gespeichert.
+                    translationToMorse.push(letterList[j].morseCode);
                 }
             }
         }
+
+        return translationToMorse;
+    }
+
+    public String translateToNormal(String morseWord) {
+
+        String translationToNormal;
+
+        for(int i = 0 ; i < morseWord.length() ; i++) {
+            for(int j = 0 ; j < 26 ; j++) {
+                //Wann entspricht der Buchstabe 
+                if(morseWord.equals(letterList[j])) {
+                    //Der Entsprechende Buchstabe des Morsecodes wird in den String gepushed.
+                    translationToNormal.push(letterList[j].name);
+                }
+            }
+        }
+
+        return translationToNormal;
     }
     
 }
