7f765438f89d54e1fd249eb8c137bf108df3b751
doctor-melamed <jakobmelamed@gmail.com>
2021-12-24 20:00:43 +0100

    Aufbau

---
+++ Alphabet.java
@@ -0,0 +1,16 @@
+public class Alphabet {
+
+    public static translateToMorse(String word) {
+
+        for (int i = 0 ; i < word.length() ; i++) {
+            for (int j = 0 ; j < 26 ; j++) {
+                
+                if(word[i].isEqualTo(letterList[j]))
+                {
+                    
+                }
+            }
+        }
+    }
+    
+}
